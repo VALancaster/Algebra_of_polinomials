f938a5be3fc30a902e31d9134c831fd4a9c9396d
VALancaster <144248381+VALancaster@users.noreply.github.com>
2025-03-17 19:09:29 +0300

    Update test_tlisttable.cpp

--- laba1/base_test/test_tlisttable.cpp
+++ laba1/base_test/test_tlisttable.cpp
@@ -1,1 +1,42 @@
- 
+#include "gtest.h"
+#include "TListTable.h"
+
+TEST(TListTable, can_create_list_table)
+{
+}
+
+TEST(TListTable, size_of_empty_table_is_zero)
+{
+}
+
+TEST(TListTable, can_insert_element)
+{
+}
+
+TEST(TListTable, insert_increases_size)
+{
+}
+
+TEST(TListTable, can_find_inserted_element)
+{
+}
+
+TEST(TListTable, finding_nonexistent_element_returns_null)
+{
+}
+
+TEST(TListTable, can_delete_inserted_element)
+{
+}
+
+TEST(TListTable, size_decreases_after_deletion)
+{
+}
+
+TEST(TListTable, deleting_nonexistent_element_does_not_change_size)
+{
+}
+
+TEST(TListTable, can_iterate_over_table_elements)
+{
+}
