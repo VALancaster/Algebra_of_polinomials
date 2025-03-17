0f7a058300eba13d0b4685818c0419999e80dbbd
VALancaster <144248381+VALancaster@users.noreply.github.com>
2025-03-17 19:10:34 +0300

    Update test_ttreetable.cpp

---
+++ laba1/base_test/test_ttreetable.cpp
@@ -0,0 +1,42 @@
+#include "gtest.h"
+#include "TTreeTable.h"
+
+TEST(TTreeTable, can_create_treetable)
+{
+}
+
+TEST(TTreeTable, size_of_empty_table_is_zero)
+{
+}
+
+TEST(TTreeTable, can_insert_element)
+{
+}
+
+TEST(TTreeTable, insert_increases_size)
+{
+}
+
+TEST(TTreeTable, can_find_inserted_element)
+{
+}
+
+TEST(TTreeTable, finding_nonexistent_element_returns_null)
+{
+}
+
+TEST(TTreeTable, can_delete_inserted_element)
+{
+}
+
+TEST(TTreeTable, size_decreases_after_deletion)
+{
+}
+
+TEST(TTreeTable, deleting_nonexistent_element_does_not_change_size)
+{
+}
+
+TEST(TTreeTable, can_handle_duplicate_keys)
+{
+}
