46a2cf7fa098d3fe4bd2d99d6b47d9ab0b2e7f4e
VALancaster <144248381+VALancaster@users.noreply.github.com>
2025-03-17 19:10:03 +0300

    Update test_tsortedarraytable.cpp

---
+++ laba1/base_test/test_tsortedarraytable.cpp
@@ -0,0 +1,42 @@
+#include "gtest.h"
+#include "TSortedArrayTable.h"
+
+TEST(TSortedArrayTable, can_create_sorted_array_table)
+{
+}
+
+TEST(TSortedArrayTable, size_of_empty_table_is_zero)
+{
+}
+
+TEST(TSortedArrayTable, can_insert_element)
+{
+}
+
+TEST(TSortedArrayTable, insert_increases_size)
+{
+}
+
+TEST(TSortedArrayTable, can_find_inserted_element)
+{
+}
+
+TEST(TSortedArrayTable, finding_nonexistent_element_returns_null)
+{
+}
+
+TEST(TSortedArrayTable, can_delete_inserted_element)
+{
+}
+
+TEST(TSortedArrayTable, size_decreases_after_deletion)
+{
+}
+
+TEST(TSortedArrayTable, deleting_nonexistent_element_does_not_change_size)
+{
+}
+
+TEST(TSortedArrayTable, maintains_sorted_order_after_insertion)
+{
+}
