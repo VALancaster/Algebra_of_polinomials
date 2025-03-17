6a2cb78a2a25d65adaa4dc2cf2141b28d063484d
VALancaster <144248381+VALancaster@users.noreply.github.com>
2025-03-17 19:08:52 +0300

    Update test_thashtableopen.cpp

--- laba1/base_test/test_thashtableopen.cpp
+++ laba1/base_test/test_thashtableopen.cpp
@@ -1,1 +1,42 @@
- 
+#include "gtest.h"
+#include "THashTableOpen.h"
+
+TEST(THashTableOpen, can_create_empty_hash_table)
+{
+}
+
+TEST(THashTableOpen, can_insert_element)
+{
+}
+
+TEST(THashTableOpen, size_increases_after_insertion)
+{
+}
+
+TEST(THashTableOpen, can_find_inserted_element)
+{
+}
+
+TEST(THashTableOpen, finding_nonexistent_element_returns_null)
+{
+}
+
+TEST(THashTableOpen, can_delete_element)
+{
+}
+
+TEST(THashTableOpen, size_decreases_after_deletion)
+{
+}
+
+TEST(THashTableOpen, deleting_nonexistent_element_does_not_change_size)
+{
+}
+
+TEST(THashTableOpen, can_handle_collisions)
+{
+}
+
+TEST(THashTableOpen, can_update_existing_key)
+{
+} 
