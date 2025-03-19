9e06d2cfd43164377b8e8bf7d309bff902d32b00
VALancaster <valentin.kutergin@gmail.com>
2025-03-19 23:12:33 +0300

    изменил несколько функций в polinom

--- laba1/LAB1/polinom.cpp
+++ laba1/LAB1/polinom.cpp
@@ -1,4 +1,4 @@
-#include "polinom.h"
+﻿#include "polinom.h"
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
@@ -12,19 +12,19 @@ Polinom::Polinom(const string& str)
 	size_t length = str.size();
 	while (pos < length) 
 	{
-		while (pos < length && isspace(str[pos])) // ??????? ???? ???????? ? ?????????
+		while (pos < length && isspace(str[pos])) // пропуск если пробелы и табуляция
 			pos++;
 		if (pos >= length) 
 			break;
-		size_t startPos = pos; // ?????? ??????
-		if (str[pos] == '+' || str[pos] == '-') // ??????? ?????
+		size_t startPos = pos; // начало строки
+		if (str[pos] == '+' || str[pos] == '-') // пропуск знака
 			pos++;
-		while (pos < length && !isspace(str[pos]) && str[pos] != '+' && str[pos] != '-') // ???? ?? ????? ???????? ??????
+		while (pos < length && !isspace(str[pos]) && str[pos] != '+' && str[pos] != '-') // пока не знаки, пробелы и табуляция
 			pos++;
-		string monomStr = str.substr(startPos, pos - startPos); // ????????? - ?????
-		if (!monomStr.empty() && monomStr.find_first_not_of(" \t") != -1) // ?? ????? ? ???????? ???? ???? ?????? - ?? ??????? ? ?? ?????????
+		string monomStr = str.substr(startPos, pos - startPos); // подстрока - моном
+		if (!monomStr.empty() && monomStr.find_first_not_of(" \t") != -1) // не пусто и все символы подстроки - не пробелы и не табуляция
 		{ 
-			Monom monom(monomStr); // ???????? ?????? ?? ??????
+			Monom monom(monomStr); // создание монома из строки
 			addMonom(monom);
 		}
 	}
@@ -32,68 +32,67 @@ Polinom::Polinom(const string& str)
 }
 
 Polinom::Polinom(const Polinom& other) {
-	for (const auto& monom : other.monoms) {
+	for (const auto& monom : other.monoms) 
+	{
 		monoms.addToEnd(monom);
 	}
 }
 
-void Polinom::removeZeroMonoms() {
+void Polinom::removeZeroMonoms() 
+{
 	size_t i = 0;
-	while (i < monomList.length()) {
-		if (monomList.getItem(i).coeff == 0.0) {
-			monomList.eraseAt(i);
-		}
-		else {
+	while (i < monoms.length()) 
+	{
+		if (monoms.getItem(i).coef == 0.0) // если коэффициент 0
+			monoms.eraseAt(i);
+		else 
 			++i;
-		}
 	}
 }
 
-void Polinom::addMonom(const Monom& monom) {
-	if (monom.coeff == 0.0) {
+void Polinom::addMonom(const Monom& monom) 
+{
+	if (monom.coef == 0.0) // если коэффициент = 0
 		return;
-	}
-
 	bool found = false;
-	for (auto& existingMonom : monomList) {
-		if (existingMonom.getDegreeHash() == monom.getDegreeHash()) {
-			existingMonom.coeff += monom.coeff;
+	for (auto& existingMonom : monoms) 
+	{
+		if (existingMonom.x == monom.x && existingMonom.y == monom.y && existingMonom.z == monom.z)
+		{
+			existingMonom.coef += monom.coef; // если степени мономов одинаковые, то суммируем коэффициенты
 			found = true;
 			break;
 		}
 	}
-
-	if (!found) {
-		monomList.addToEnd(monom);
-	}
+	if (!found) 
+		monoms.addToEnd(monom);
 }
 
-double Polinom::evaluate(double x, double y, double z) const {
+double Polinom::evaluate(double x, double y, double z) const 
+{
 	double result = 0.0;
-	for (auto& monom : monomList) {
+	for (auto& monom : monoms) 
 		result += monom.evaluate(x, y, z);
-	}
 	return result;
 }
 
-std::string Polinom::toString() const {
-	std::string result = "";
+string Polinom::toString() const {
+	string result = ""; // сначала пустая строка
 
-	for (size_t i = 0; i < monomList.length(); ++i) {
-		const Monom& monom = monomList.getItem(i);
-		std::string monomStr = monom.toString();
-
-		if (!monomStr.empty()) {
-			if (result.empty()) {
+	for (size_t i = 0; i < monoms.length(); ++i) 
+	{
+		const Monom& monom = monoms.getItem(i); // берется моном из списка
+		string monomStr = monom.toString(); // каждый моном превращается в строку
+		if (!monomStr.empty()) 
+		{
+			if (result.empty()) // текущий моном - первый
 				result = monomStr;
-			}
-			else {
-				result += monomStr[0] == '-' ? monomStr : "+" + monomStr;
-			}
+			else 
+				result += monomStr[0] == '-' ? monomStr : "+" + monomStr; // если моном начинается с минуса, он добавляется как есть
+				// если моном начинается с плюса, перед ним добавляется знак +
 		}
 	}
-
-	return result.empty() ? "0" : result;
+	return result.empty() ? "0" : result; // если строка осталась пустой - вернется '0', иначе полином
 }
 
 Polinom Polinom::derivative(char var) const {
