c189cec603a9a47e92e65c4e3e31147fe9407985
VALancaster <valentin.kutergin@gmail.com>
2025-03-22 02:21:08 +0300

    Изменил стек

--- laba1/LAB1/stack.h
+++ laba1/LAB1/stack.h
@@ -6,29 +6,36 @@ using namespace std;
 template <class T>
 class TStack
 {
-	int top;
-	size_t size;
+private:
 	T* pMem;
-
+	size_t size;
+	int top;
 public:
-	TStack() : top(-1), size(1), pMem(new T[size]) {}
-
-	~TStack() {
-		delete[] pMem;
+	TStack(int sz = 1) 
+	{
+		size = sz;
+		top = -1;
+		if (sz < 1)
+			throw out_of_range("Stack size should be greater or equal than 1");
+		pMem = new T[size];
 	}
-
 	TStack(const TStack&) = delete;
-	TStack& operator=(const TStack&) = delete;
-
-	TStack(TStack&& other) noexcept {
+	TStack(TStack&& other) noexcept 
+	{
 		other.pMem = nullptr;
 		other.top = -1;
 		other.size = 0;
 	}
 
+	~TStack() 
+	{
+		delete[] pMem;
+	}
 
-	TStack& operator=(TStack&& other) noexcept {
-
+	
+	TStack& operator=(const TStack&) = delete;
+	TStack& operator=(TStack&& other) noexcept 
+	{
 		if (this != &other)
 		{
 			delete[] pMem;
@@ -40,25 +47,30 @@ public:
 			other.size = 0;
 		}
 		return *this;
-
 	}
   
-	size_t GetSize() const {
-		return top + 1;
-	}
-
-	bool IsEmpty() const {
+	size_t GetSize() const 
+	{
+		return size;
+	}	
+	bool IsEmpty() const 
+	{
 		return top == -1;
 	}
+	bool IsFull() const
+	{
+		return top == size - 1;
+	}
 
-	T Pop() {
+	T Pop() 
+	{
 		if (IsEmpty())
 			throw out_of_range("Stack is empty");
 		return pMem[top--];
 	}
-
-	void Push(const T& val) {
-		if (top == size - 1)
+	void Push(const T& val) 
+	{
+		if (IsFull())
 		{
 			size *= 2;
 			T* tmp = new T[size];
@@ -67,6 +79,12 @@ public:
 			pMem = tmp;
 		}
 		pMem[++top] = val;
+	}
+	T GetTop()
+	{
+		if (IsEmpty())
+			throw out_of_range("Stack is empty");
+		return pMem[top];
 	}
 };
 
