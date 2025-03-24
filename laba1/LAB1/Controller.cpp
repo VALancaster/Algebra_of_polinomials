a2eb22ff46d968347fb30bb46d7b887e85517970
VALancaster <valentin.kutergin@gmail.com>
2025-03-24 19:20:02 +0300

    Добавил контроллер

---
+++ laba1/LAB1/Controller.cpp
@@ -0,0 +1,44 @@
+﻿#include "Controller.h"
+
+using namespace std;
+
+void Controller::SetActiveTable(TTable* table)
+{
+	if (table)
+		active_table = table;
+	else
+	{
+		cerr << "Ошибка: передана пустая таблица." << endl;
+		return;
+	}
+}
+
+void Controller::AddPolinom(const string& name, const Polinom& pol)
+{
+	if (active_table)
+		active_table->Insert(name, pol);
+	else
+	{
+		cerr << "Ошибка: активная таблица не установлена." << endl;
+		return;
+	}
+}
+
+Polinom Controller::EvaluateExpression(const string& expression)
+{
+	if (!active_table)
+		throw runtime_error("Ошибка: активная таблица не установлена.");
+	postfix_processor.SetInfix(expression); // ???
+	return postfix_processor.Calculate(*active_table); // ???
+}
+
+void Controller::PrintActivetable()
+{
+	if (active_table)
+		active_table->Print();
+	else
+	{
+		cerr << "Ошибка: активная таблица не установлена." << endl;
+		return;
+	}
+}
--- laba1/LAB1/Controller.h
+++ laba1/LAB1/Controller.h
@@ -4,6 +4,7 @@
 #include "TTable.h"
 #include "Postfix.h"
 #include <vector>
+#include <iostream>
 
 class Controller // контроллер таблиц
 {
@@ -11,6 +12,7 @@ class Controller // контроллер таблиц
 	TTable* active_table;
 	TPostfix postfix_processor;
 public:
+	Controller(): active_table(nullptr) {}
 	void SetActiveTable(TTable* table);
 	void AddPolinom(const std::string& name, const Polinom& pol);
 	Polinom EvaluateExpression(const std::string& expression);
