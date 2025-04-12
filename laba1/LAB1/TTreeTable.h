6d6f46766672cf80d80defc47530a68a4fcad130
VALancaster <valentin.kutergin@gmail.com>
2025-04-13 02:07:48 +0300

    ОТредактировал таблицу

--- laba1/LAB1/TTreeTable.h
+++ laba1/LAB1/TTreeTable.h
@@ -1,5 +1,4 @@
-﻿
-#ifndef TTREETABLE_H
+﻿#ifndef TTREETABLE_H
 #define TTREETABLE_H
 
 #include "TTable.h"
@@ -9,7 +8,8 @@
 template <typename TKey, typename TValue>
 class TTreeTable : public TTable<TKey, TValue> 
 {
-    struct Node {
+    struct Node 
+    {
         TKey key;
         TValue value;
         Node* left = nullptr;
@@ -37,118 +37,149 @@ class TTreeTable : public TTable<TKey, TValue>
 
     Node* RotateRight(Node* y) 
     {
-        Node* x = y->left;
-        y->left = x->right;
-        x->right = y;
-        UpdateHeight(y);
+        Node* x = y->left; // Запоминаем левый потомок (новый корень)
+        y->left = x->right; // Перемещаем правое поддерево x на место левого поддерева y
+        x->right = y; // Старый корень y становится правым потомком x
+        UpdateHeight(y); 
         UpdateHeight(x);
-        return x;
+        return x; // Возвращаем новый корень поддерева
     }
 
     Node* RotateLeft(Node* x) 
     {
-        Node* y = x->right;
-        x->right = y->left;
-        y->left = x;
+        Node* y = x->right; // Запоминаем правый потомок (новый корень)
+        x->right = y->left; // Перемещаем левое поддерево y на место правого поддерева x
+        y->left = x; // Старый корень x становится левым потомком y
         UpdateHeight(x);
         UpdateHeight(y);
-        return y;
+        return y; // Возвращаем новый корень поддерева
     }
 
     Node* Balance(Node* node) 
     {
-        UpdateHeight(node);
-        int bf = BalanceFactor(node);
+        UpdateHeight(node); // Обновляем высоту текущего узла
+        int bf = BalanceFactor(node); // баланс-фактор
 
-        if (bf > 1) {
-            if (BalanceFactor(node->left) < 0) {
+        if (bf > 1) // левый перевес
+        {
+            if (BalanceFactor(node->left) < 0) // перевес в левое-правое поддерево
+            {
                 node->left = RotateLeft(node->left);
             }
-            return RotateRight(node);
+            return RotateRight(node); // перевес в левое-левое поддерево
         }
-        if (bf < -1) {
-            if (BalanceFactor(node->right) > 0) {
+        if (bf < -1) // правый перевес
+        {
+            if (BalanceFactor(node->right) > 0) // перевес в правое-левое поддерево
+            {
                 node->right = RotateRight(node->right);
             }
-            return RotateLeft(node);
+            return RotateLeft(node); // перевес в правое-правое поддерево
         }
-        return node;
+        return node; // балансировка не требуется
     }
 
     Node* Insert(Node* node, const TKey& key, const TValue& value) 
     {
-        if (!node) {
+        if (!node) 
+        {
             count++;
             return new Node{ key, value };
         }
-
-        if (key < node->key) {
+        if (key < node->key) 
+        {
             node->left = Insert(node->left, key, value);
         }
-        else if (key > node->key) {
+        else if (key > node->key) 
+        {
             node->right = Insert(node->right, key, value);
         }
-        else {
+        else 
+        {
             node->value = value;
             return node;
         }
-
         return Balance(node);
     }
 
     Node* FindMin(Node* node) const 
     {
-        while (node->left) node = node->left;
+        while (node->left)
+        {
+            node = node->left;
+        }
         return node;
     }
 
     Node* Remove(Node* node, const TKey& key) 
     {
-        if (!node) return nullptr;
-
-        if (key < node->key) {
+        if (!node)
+        {
+            return nullptr;
+        }
+        if (key < node->key) 
+        {
             node->left = Remove(node->left, key);
         }
-        else if (key > node->key) {
+        else if (key > node->key) 
+        {
             node->right = Remove(node->right, key);
         }
-        else {
-            if (!node->left || !node->right) {
+        else 
+        {
+            if (!node->left || !node->right) // лист
+            {
                 Node* temp = node->left ? node->left : node->right;
-                if (!temp) {
+                if (!temp) 
+                {
                     temp = node;
                     node = nullptr;
                 }
-                else {
+                else 
+                {
                     *node = *temp;
                 }
                 delete temp;
                 count--;
             }
-            else {
+            else 
+            {
                 Node* minRight = FindMin(node->right);
                 node->key = minRight->key;
                 node->value = minRight->value;
                 node->right = Remove(node->right, minRight->key);
             }
         }
-
-        if (!node) return nullptr;
+        if (!node)
+        {
+            return nullptr;
+        }
         return Balance(node);
     }
 
     Node* FindNode(Node* node, const TKey& key) const 
     {
-        if (!node) return nullptr;
-        if (key < node->key) return FindNode(node->left, key);
-        if (key > node->key) return FindNode(node->right, key);
+        if (!node)
+        {
+            return nullptr;
+        }
+        if (key < node->key)
+        {
+            return FindNode(node->left, key);
+        }
+        if (key > node->key)
+        {
+            return FindNode(node->right, key);
+        }
         return node;
     }
 
     void Print(Node* node) const 
     {
-        if (!node) 
+        if (!node)
+        {
             return;
+        }
         Print(node->left);
         cout << "Key: " << node->key << ", Value: " << node->value << endl;
         Print(node->right);
@@ -207,223 +238,3 @@ public:
 };
 
 #endif
-
-/*
-#ifndef TTREETABLE_H
-#define TTREETABLE_H
-
-#include "TTable.h"
-#include <stdexcept>
-
-template <typename TKey, typename TValue>
-class TTreeTable : public TTable<TKey, TValue> // Таблица с использованием AVL-дерева.
-{
-    struct TTableRec
-    {
-        TKey key;
-        TValue value;
-    };
-
-    struct TreeNode // Узел дерева
-    {
-        TTableRec data;
-        TreeNode* pLeft = nullptr;
-        TreeNode* pRight = nullptr;
-        int height = 1; // Высота узла
-    };
-
-    TreeNode* pRoot = nullptr;
-    size_t treeSize = 0;
-
-    int Height(TreeNode* pNode)
-    {
-        return (pNode == nullptr) ? 0 : pNode->height;
-    }
-
-    int BalanceFactor(TreeNode* pNode)
-    {
-        return (pNode == nullptr) ? 0 : Height(pNode->pLeft) - Height(pNode->pRight);
-    }
-
-    void UpdateHeight(TreeNode* pNode)
-    {
-        if (pNode != nullptr) {
-            pNode->height = 1 + std::max(Height(pNode->pLeft), Height(pNode->pRight));
-        }
-    }
-
-    TreeNode* RotateRight(TreeNode* pNode)
-    {
-        TreeNode* newRoot = pNode->pLeft;
-        pNode->pLeft = newRoot->pRight;
-        newRoot->pRight = pNode;
-
-        UpdateHeight(pNode);
-        UpdateHeight(newRoot);
-
-        return newRoot;
-    }
-
-    TreeNode* RotateLeft(TreeNode* pNode)
-    {
-        TreeNode* newRoot = pNode->pRight;
-        pNode->pRight = newRoot->pLeft;
-        newRoot->pLeft = pNode;
-
-        UpdateHeight(pNode);
-        UpdateHeight(newRoot);
-
-        return newRoot;
-    }
-
-    TreeNode* Balance(TreeNode* pNode)
-    {
-        UpdateHeight(pNode);
-
-        int bf = BalanceFactor(pNode);
-
-        if (bf > 1) // Левое поддерево выше
-        {
-            if (BalanceFactor(pNode->pLeft) < 0) {
-                pNode->pLeft = RotateLeft(pNode->pLeft);
-            }
-            return RotateRight(pNode);
-        }
-        if (bf < -1) // Правое поддерево выше
-        {
-            if (BalanceFactor(pNode->pRight) > 0) {
-                pNode->pRight = RotateRight(pNode->pRight);
-            }
-            return RotateLeft(pNode);
-        }
-        return pNode;
-    }
-
-    TreeNode* InsertNode(TreeNode* pNode, TKey key, TValue value)
-    {
-        if (pNode == nullptr) {
-            treeSize++;
-            return new TreeNode{ {key, value}, nullptr, nullptr, 1 };
-        }
-
-        if (key < pNode->data.key) {
-            pNode->pLeft = InsertNode(pNode->pLeft, key, value);
-        }
-        else if (key > pNode->data.key) {
-            pNode->pRight = InsertNode(pNode->pRight, key, value);
-        }
-        else {
-            pNode->data.value = value; // Обновление значения при совпадении ключа
-            return pNode;
-        }
-
-        return Balance(pNode);
-    }
-
-    TreeNode* DeleteNode(TreeNode* pNode, TKey key)
-    {
-        if (pNode == nullptr) {
-            return nullptr;
-        }
-
-        if (key < pNode->data.key) {
-            pNode->pLeft = DeleteNode(pNode->pLeft, key);
-        }
-        else if (key > pNode->data.key) {
-            pNode->pRight = DeleteNode(pNode->pRight, key);
-        }
-        else 
-        {
-            if (pNode->pLeft == nullptr || pNode->pRight == nullptr) {
-                TreeNode* tmp = pNode->pLeft ? pNode->pLeft : pNode->pRight;
-                delete pNode;
-                treeSize--;
-                return tmp;
-            }
-
-            TreeNode* minNode = FindMin(pNode->pRight);
-            pNode->data = minNode->data;
-            pNode->pRight = DeleteNode(pNode->pRight, minNode->data.key);
-        }
-        return Balance(pNode);
-    }
-
-    TreeNode* FindNode(TKey key, TreeNode* pNode)
-    {
-        if (pNode == nullptr)
-            return nullptr;
-        if (key < pNode->data.key)
-            return FindNode(key, pNode->pLeft);
-        if (key > pNode->data.key)
-            return FindNode(key, pNode->pRight);
-        return pNode;
-    }
-
-    TreeNode* FindMin(TreeNode* pNode)
-    {
-        while (pNode->pLeft != nullptr)
-            pNode = pNode->pLeft;
-        return pNode;
-    }
-
-    void Print(TreeNode* p)
-    {
-        if (p == nullptr)
-            return;
-        Print(p->pLeft);
-        std::cout << p->data.key << "-" << p->data.value << std::endl;
-        Print(p->pRight);
-    }
-
-public:
-    TTreeTable() = default;
-
-    ~TTreeTable()
-    {
-        while (pRoot != nullptr)
-            pRoot = DeleteNode(pRoot, pRoot->data.key);
-    }
-
-    size_t size() const noexcept
-    {
-        return treeSize;
-    }
-
-    TValue& operator[](size_t pos)
-    {
-        throw std::out_of_range("Operator[] not supported for tree traversal");
-    }
-
-    void Print()
-    {
-        std::cout << "Table\n";
-        Print(pRoot);
-    }
-
-    string GetName() const override
-    {
-        return "Tree Table";
-    }
-
-    void Delete(TKey key)
-    {
-        pRoot = DeleteNode(pRoot, key);
-    }
-
-    TValue* Find(TKey key)
-    {
-        TreeNode* pNode = FindNode(key, pRoot);
-        if (pNode == nullptr)
-            return nullptr;
-        return &pNode->data.value;
-    }
-
-    void Insert(TKey key, TValue value)
-    {
-        pRoot = InsertNode(pRoot, key, value);
-    }
-};
-
-#endif
-*/
-
